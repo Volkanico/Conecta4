0d9c45a2416147773e1261f72eac95c6695d74b7
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2022-04-10 22:55:39 +0200

    Limpieza

--- src/com/company/InputOutput.java
+++ src/com/company/InputOutput.java
@@ -1,5 +1,4 @@
 package com.company;
-
 import java.util.Scanner;
 
 public class InputOutput {
@@ -49,8 +48,6 @@ public class InputOutput {
                 System.out.println(e);
             }
         }
-        verificador = false;
-
     }
 
     public void opcionesDeJuego () {
@@ -69,7 +66,6 @@ public class InputOutput {
         if (jugador1.equals(null) || jugador1.equals("")) {
             jugador1 = "Jugador 1";
         }
-
         jugador1 = "\033[32m" + jugador1 + "\u001B[0m";
 
         System.out.println(jugador1 + " ingrese un caracter para usarlo en el juego (Puede ser un signo, numero, letra, etc)");
@@ -88,7 +84,6 @@ public class InputOutput {
         if (jugador2.equals(null) || jugador2.equals("")) {
             jugador2 = "Jugador 2";
         }
-
         jugador2 = "\033[36m" + jugador2 + "\u001B[0m";
 
         System.out.println(jugador2 + " ingrese un caracter para usarlo en el juego (Puede ser un signo, numero, letra, etc)");
@@ -104,9 +99,7 @@ public class InputOutput {
             mensajeError = "";
             partidaTurnos();
         }
-
     }
-
 
     public void controlErrorColumnasJugador1YsuJuego () {
         try {
@@ -139,12 +132,9 @@ public class InputOutput {
         } catch (Exception e) {
             mensajeError = "\n\033[35m**ERROR:\u001B[0m Debe ingresar un numero entre el 1 al " + tablero.getCapacidadColumnas();
         }
-
         verificador2 = false;
         verificador = false;
-        auxiliarJugador = jugador1;
         auxiliarJugador = caracter1;
-        //llamo a la funcion "verificarGanador" para controlar si gano el jugador 1
         tablero.verificadorGanador(jugador1, caracter1);
         tablero.mostrarTablero(tablero.getTablero());
         if (jugadasMaximas == (tablero.getCapacidadFilas() * tablero.getCapacidadColumnas()) + 1) {
@@ -155,13 +145,10 @@ public class InputOutput {
             System.out.println("El tablero esta lleno");
             System.out.println("======================");
         }
-
         //Si el jugador 1 no gano en su turno continua con el jugador 2
         if (!tablero.isFinJuego()) {
-
             //turno jugador 2
             while (!verificador) {
-                //llamo a la funcion "mostrarTablero" y "mostrarInterfazTablero" de la clase conecta 4;
                 tablero.getNumColumnas(tablero);
                 tablero.mostrarInterfazTablero(jugador1, caracter1, jugador2, caracter2, jugadasMaximas);
                 System.out.println(mensajeError);
@@ -201,9 +188,7 @@ public class InputOutput {
             verificador = false;
             auxiliarJugador = jugador2;
             auxiliarJugador = caracter2;
-            //llamo a la funcion "verificarGanador" para controlar si gano el jugador 2
             tablero.verificadorGanador(jugador2, caracter2);
-
 
             if (jugadasMaximas == (tablero.getCapacidadFilas() * tablero.getCapacidadColumnas()) + 1) {
                 tablero.setFinJuego(true);
@@ -214,9 +199,7 @@ public class InputOutput {
                 System.out.println("======================");
             }
         }
-
     }
-
 
     public void posiblesErroresEnCaracterDeJugador1 (){
         while (caracter1.length() > 1 || caracter1.equals(null) || caracter1.equals("")) {
@@ -258,12 +241,9 @@ public class InputOutput {
         caracter1 = "\033[31m" + caracter1 + "\u001B[0m";
         caracter2 = "\033[34m" + caracter2 + "\u001B[0m";
     }
-
     public void partidaTurnos () {
         controlErrorColumnasJugador1YsuJuego();
-
     }
-
     public void juego () {
         titol();
         comprobarEntradaDificultad();
